d37108ff90caaa202ed24fc8eadc57f776080f55
Danny Nothdurft <d.nothdurft@gmail.de>
2022-02-01 17:17:00 +0100

    basis Fertig

--- assets/scripte/index.js
+++ assets/scripte/index.js
@@ -9,19 +9,48 @@ const number1 = document.getElementById('number1'),
       number9 = document.getElementById('number9'),
       number0 = document.getElementById('number0'),
       anzeige = document.getElementById('anzeige'),
+      aufgabe = document.getElementById('aufgabe'),
       reset = document.getElementById('reset'),
-      enter = document.getElementById('enter');
+      enter = document.getElementById('enter'),
+      minus = document.getElementById('minus');
+let ergebnis = 0;
 
 // Zufallzahl generieren und ausgeben
+function randomLv1() {
+    const random1 = Math.round(Math.random() * 10 + 0.5),
+          random2 = Math.round(Math.random() * 10 + 0.5),
+          operator = ['+', '-'],
+          random3 = Math.floor(Math.random() * operator.length),
+          ausgabe = operator[random3];
 
-function random() {
-    const random1 = Math.round(Math.random()*10 + 0.5),
-          random2 = Math.round(Math.random()*10 + 0.5);
+    if ( random3 === 0) {
+        aufgabe.innerHTML = `${random1} + ${random2}`;
+        return ergebnis = random1 + random2;
+    } else if ( random3 === 1 ) {
+        aufgabe.innerHTML = `${random1} - ${random2}`;
+        return ergebnis = random1 - random2;
+    };
+};
 
-    return(`${random1} + ${random2}`);
+// Ergebnis eingeben und zwischen speichern
+function meinErgebnis() {
+    let vergleich = ergebnis === parseFloat(anzeige.innerText);
+    
+    if ( vergleich === true ) {
+        aufgabe.innerHTML = 'Richtig';
+        anzeige.innerHTML = '';
+        setTimeout( randomLv1, 2000 );
+    } if ( vergleich === false ) {
+        aufgabe.innerHTML = `${ergebnis}`;
+        anzeige.innerHTML = '';
+        setTimeout( randomLv1, 2000 );
+    }
 }
 
-// Zahlen in der Anzeige ausgeben
+enter.addEventListener('click', meinErgebnis);
+
+
+// Zahlen in der Anzeige eingeben
 number1.addEventListener('click', (event) => {
     anzeige.innerHTML += 1;
 });
@@ -60,6 +89,10 @@ number9.addEventListener('click', (event) => {
 
 number0.addEventListener('click', (event) => {
     anzeige.innerHTML += 0;
+});
+
+minus.addEventListener('click', (event) => {
+    anzeige.innerHTML += "-";
 });
 
 // Anzeige Reset
