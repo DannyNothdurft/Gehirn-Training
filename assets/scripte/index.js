5f4489a256663c1c3917c2044d07a8d92374e00b
Danny Nothdurft <d.nothdurft@gmail.de>
2022-02-03 11:01:12 +0100

    Beta Version ohne Localhost

--- assets/scripte/index.js
+++ assets/scripte/index.js
@@ -8,21 +8,72 @@ const number1 = document.getElementById('number1'),
       number8 = document.getElementById('number8'),
       number9 = document.getElementById('number9'),
       number0 = document.getElementById('number0'),
+      overlay = document.getElementById('overlay'),
       anzeige = document.getElementById('anzeige'),
       aufgabe = document.getElementById('aufgabe'),
+      timer = document.getElementById('timer'),
+      start = document.getElementById('start'),
       reset = document.getElementById('reset'),
       enter = document.getElementById('enter'),
-      minus = document.getElementById('minus');
-let ergebnis = 0;
+      minus = document.getElementById('minus'),
+      epBar = document.getElementById('ep'),
+      lvBar = document.getElementById('lv'),
+      lpBar = document.getElementById('lp');
+
+let isPaused = true,
+    ergebnis = 0,
+    time = 10,
+    ep = 0,
+    lv = 0,
+    lp = 10;
+
+let timerGo = setInterval(() => {
+    if(!isPaused && time >= 0) {
+        timer.innerHTML = parseInt(time % 60, 10);
+        time--;
+        console.log(time);
+    } else {
+        timerPause();
+    };
+}, 1000);
+
+function timerPause() {
+    isPaused = true;
+    timer.innerHTML = 10;
+    if (time <= 0 ){
+        aufgabe.innerHTML = 'Zeit ist um!';
+        time = 10;
+        lpMinus();
+        start.classList.remove('dialog');
+        overlay.classList.add('body-overlay');
+    }
+};
+
+function timerStart(){
+    isPaused = false;
+};
+
+// Lebenspunkte abzug
+function lpMinus(){
+    lp--;
+    anzeigeBar();
+}
+
+// Anzeige von EP, LV & LP
+anzeigeBar();
+function anzeigeBar(){
+    epBar.innerHTML = ep;
+    lvBar.innerHTML = lv;
+    lpBar.innerHTML = lp;
+};
 
 // Zufallzahl generieren und ausgeben
 function randomLv1() {
     const random1 = Math.round(Math.random() * 10 + 0.5),
           random2 = Math.round(Math.random() * 10 + 0.5),
           operator = ['+', '-'],
-          random3 = Math.floor(Math.random() * operator.length),
-          ausgabe = operator[random3];
-
+          random3 = Math.floor(Math.random() * operator.length);
+         
     if ( random3 === 0) {
         aufgabe.innerHTML = `${random1} + ${random2}`;
         return ergebnis = random1 + random2;
@@ -35,20 +86,35 @@ function randomLv1() {
 // Ergebnis eingeben und zwischen speichern
 function meinErgebnis() {
     let vergleich = ergebnis === parseFloat(anzeige.innerText);
+    time = 10;
     
-    if ( vergleich === true ) {
-        aufgabe.innerHTML = 'Richtig';
-        anzeige.innerHTML = '';
+        if ( vergleich === true ) {
+            aufgabe.innerHTML = 'Richtig';
+            anzeige.innerHTML = '';
+            ep++
+        } if ( vergleich === false ) {
+            aufgabe.innerHTML = `${ergebnis}`;
+            anzeige.innerHTML = '';
+            lp--
+        }
+        anzeigeBar();
+        timerPause();
+        setTimeout( timerStart, 1000 );
         setTimeout( randomLv1, 2000 );
-    } if ( vergleich === false ) {
-        aufgabe.innerHTML = `${ergebnis}`;
-        anzeige.innerHTML = '';
-        setTimeout( randomLv1, 2000 );
-    }
 }
 
 enter.addEventListener('click', meinErgebnis);
 
+// Spiel Start
+function gameStart(e) {
+    e.preventDefault();
+    start.classList.add('dialog');
+    overlay.classList.remove('body-overlay');
+    isPaused = false;
+    randomLv1();
+}
+
+start.addEventListener('click', gameStart);
 
 // Zahlen in der Anzeige eingeben
 number1.addEventListener('click', (event) => {
@@ -99,8 +165,4 @@ minus.addEventListener('click', (event) => {
 
 reset.addEventListener('click', (event) => {
     anzeige.innerHTML = "";
-})
-
-
-// test console
-console.log(random());
+});
